33c3d0458f5c47cfddf1c1c3fb421d630c3ff7c8
q7zn <33761450+q7zn@users.noreply.github.com>
2017-11-28 09:42:45 -0800

    Initial commit

---
+++ notes.py
@@ -0,0 +1,34 @@
+print("Hello, world!")
+
+# DJ Stewart
+
+a = 4
+b = 3
+
+print(3 + 5)
+print(5 - 3)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 2)
+
+print()
+print("Try to figure out how this works")
+print(13 % 5)
+
+car_name = "The Dug-Mobile"
+car_type = "Bugatti"
+car_cylinders = 16
+car_mpg = 5000.9
+
+print("I have a car called %s. It's pretty nice." % car_name)
+print("I have a car called %s. It's a %s." % (car_name, car_type))
+
+print("What is your name?")
+name = input(">_")
+print("Hello %s." % name)
+
+age = input("How old are you?")
+print("%s?! That's really old. You belong in a retirement home." % age)
+
+#this is a new line
+#this is another new line because Github was being weird
