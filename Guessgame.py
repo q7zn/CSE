ffdf6f1b371df1a5d3ef0bbc42349cd9c0d6a26f
q7zn <33761450+q7zn@users.noreply.github.com>
2017-12-05 09:45:49 -0800

    completed and fixed guessgame

--- Guessgame.py
+++ Guessgame.py
@@ -2,32 +2,33 @@ import random
 
 guessesTaken = 0
 
-print('Hello! What is your name?')
+print('Oh, hello there! What is your name?')
 myName = input()
-
+global guess
 number = random.randint(1, 50)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+print(myName + "?!?! " + "That's a terrible name! I would feel bad for you, but I'm not programmed to."
+               " Anyway, " + myName + ", I am thinking of a number between 1 and 50.")
 
-while guessesTaken < 3:
-    print("Take a gander at the number I'm thinking of.") # There are four spaces in front of print.
+while guessesTaken < 5:
+    print("Take a shot at the number I'm thinking of.")
     guess = input()
     guess = int(guess)
 
     guessesTaken = guessesTaken + 1
 
     if guess < number:
-         print('Your guess is too low.')
+        print('Go higher. Your guess is too low.')
 
     if guess > number:
-         print('Your guess is too high.')
+        print('Overshot it there, go lower. Your guess is too high.')
 
     if guess == number:
-         break
+        break
 
     if guess == number:
         guessesTaken = str(guessesTaken)
         print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
 
 if guess != number:
-     number = str(number)
+    number = str(number)
 print('Nope. The number I was thinking of was ' + number)
--- Warmups.py
+++ Warmups.py
@@ -1,4 +1,4 @@
-def reverse_order():
+'''def reverse_order():
     first_name: input()
     last_name: input()
 
@@ -6,3 +6,7 @@ def reverse_order():
 print(last_name)
 print(" ")
 print(first_name)
+'''
+
+def add_py(name):
+    print(name + ".py")
