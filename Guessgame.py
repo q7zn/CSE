4a464979d893108e941f431ec16987cdb7c5a139
q7zn <33761450+q7zn@users.noreply.github.com>
2017-12-06 09:45:56 -0800

    buggy finish, guessgame

--- Guessgame.py
+++ Guessgame.py
@@ -4,10 +4,11 @@ guessesTaken = 0
 
 print('Oh, hello there! What is your name?')
 myName = input()
-global guess
 number = random.randint(1, 50)
 print(myName + "?!?! " + "That's a terrible name! I would feel bad for you, but I'm not programmed to."
                " Anyway, " + myName + ", I am thinking of a number between 1 and 50.")
+
+global guess
 
 while guessesTaken < 5:
     print("Take a shot at the number I'm thinking of.")
@@ -22,13 +23,14 @@ while guessesTaken < 5:
     if guess > number:
         print('Overshot it there, go lower. Your guess is too high.')
 
-    if guess == number:
+'''    if guess == number:
         break
-
+'''
     if guess == number:
         guessesTaken = str(guessesTaken)
-        print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+        print('Good job, ' + myName + '! God knows how many times it took you to run this code to '
+                                      'actually beat the game. You guessed my number in ' + guessesTaken + ' guesses!')
 
 if guess != number:
     number = str(number)
-print('Nope. The number I was thinking of was ' + number)
+    print("Nope. Turns out you're a terrible guesser. The number I was thinking of was " + number)
--- Warmups.py
+++ Warmups.py
@@ -10,3 +10,18 @@ print(first_name)
 
 def add_py(name):
     print(name + ".py")
+
+
+add_py("your mom")
+
+"""Write a function called 'add'
+which takes three parameters
+and prints the sum of the numbers
+"""
+
+def add(number_1, number_2, number_3):
+    print(number_1 + number_2 + number_3)
+
+
+add(15, 18, 9000)
+add(80, 90, 100)
