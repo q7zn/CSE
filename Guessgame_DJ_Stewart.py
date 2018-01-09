18de6b1480884d4e5f0e32266275bf495da25e67
q7zn <q7zn@wetigers.org>
2018-01-09 09:45:44 -0800

    started lucky 7s

--- Guessgame_DJ_Stewart.py
+++ Guessgame_DJ_Stewart.py
@@ -18,7 +18,7 @@ while int(guessesTaken) < 5:
 
     if guess > 50:
         print("Dude, I said one through fifty! Is it really that hard to hit two different numbers that are almost "
-              "all the way across the keyboard from each other? You should be playing \na typing game instead...")
+              "all the way across the keyboard from each other? You should be playing \na typing games instead...")
 
     if guess < 1:
         print("C'mon, man, I said one through fifty. You can't guess lower than 1 by accident! Get with the program.")
---
+++ Lucky_7s_DJ_Stewart.py
@@ -0,0 +1,12 @@
+import random
+
+print ("Welcome to Lucky Sevens. You have fifteen dollars, and you must place a bet of one dollar every round.")
+print ("You will then roll two die with six sides, and if the numbers add up to seven, you win five dollars.")
+print("Let's get started. Good luck!")
+
+def money:
+    (money) = 15
+
+
+    round_lost = money - 1
+    if round_lost then money
