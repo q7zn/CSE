57ab55107871db3b512991f5c06035ffc2889e0d
q7zn <q7zn@wetigers.org>
2018-01-11 09:46:19 -0800

    really buggy, rolls same # every time

--- Lucky_7s_DJ_Stewart.py
+++ Lucky_7s_DJ_Stewart.py
@@ -1,12 +1,27 @@
 import random
 
-print ("Welcome to Lucky Sevens. You have fifteen dollars, and you must place a bet of one dollar every round.")
-print ("You will then roll two die with six sides, and if the numbers add up to seven, you win five dollars.")
+print("Welcome to Lucky Sevens. You have fifteen dollars, and you must place a bet of one dollar every round.")
+print("You will then roll two die with six sides, and if the numbers add up to seven, you win five dollars.")
 print("Let's get started. Good luck!")
 
-def money:
-    (money) = 15
+money_owned = 15
 
 
-    round_lost = money - 1
-    if round_lost then money
+def roll():
+    return random.randint(1, 6)
+
+
+dice_1 = roll()
+dice_2 = roll()
+
+while money_owned > 0:
+    print("Okay. You have %s dollars. I'll take a dollar, now roll your die. Go ahead and roll." % money_owned)
+    if (dice_1 + dice_2) == 7:
+        print("Nice! you rolled a seven! Here's five dollars, since you won the round.")
+        money_owned += 5
+    elif money_owned != 7:
+        print("Dang, that's a bummer. You rolled a %s. Roll again." % (dice_1 + dice_2))
+        roll()
+    money_owned -= 1
+    if money_owned == 0:
+        print("Congratulations! You managed to blow all your money gambling. You lost your money in %s rounds.")
